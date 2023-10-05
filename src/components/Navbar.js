ff932ada1adaa8191ca2535bd094883ba16ca1e9
Nand Raj <nandraj2001@gmail.com>
2023-10-05 06:55:23 +0530

    integrations of landing page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import logo from "../resources/logo.png";
 import ieeeLogo from "../resources/ieee_logo.png";
+import { CardText, CardTitle } from 'react-bootstrap';
 const Navbar = () => {
     return (
         <React.Fragment>
-            <img src={logo} alt="Logo" className="logo" />
-
-            <div className="heading">
-                <h1>IEEE Student Branch NIT Jamshedpur</h1>
-                <p style={{ marginTop: '-2%' }}>
-                    The professional home for the engineering <br></br> and technology community at
-                    NIT Jamshedpur
-                </p>
+            <div className='head-wrapper'>
+                <img src={logo} alt="Logo" className="logo" />
+                <div className="heading">
+                    <CardTitle className="fs-1">IEEE Student Branch NIT Jamshedpur</CardTitle>
+                    <CardText style={{ marginTop: '-2%' }} className="fs-3">
+                        The professional home for the engineering <br></br> and technology community at
+                        NIT Jamshedpur
+                    </CardText>
+                </div>
+                <img src={ieeeLogo} alt="IEEE Logo" className="ieee-logo" />
             </div>
-            <img src={ieeeLogo} alt="IEEE Logo" className="ieee-logo" />
-
-            <div className="buttons">
+            <div className="container buttons">
                 <button><u style={{ textUnderlinePosition: 'under' }}>About</u></button>
                 <button><u style={{ textUnderlinePosition: 'under' }}>Events</u></button>
                 <button><u style={{ textUnderlinePosition: 'under' }}>Branch Chapters</u></button>
                 <button><u style={{ textUnderlinePosition: 'under' }}>Resources</u></button>
                 <button><u style={{ textUnderlinePosition: 'under' }}>Membership</u></button>
                 <button><u style={{ textUnderlinePosition: 'under' }}>Contact Us</u></button>
-            </div></React.Fragment>
+            </div>
+        </React.Fragment>
 
     )
 }

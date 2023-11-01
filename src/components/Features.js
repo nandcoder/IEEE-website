60f7fba8952eaf6021a0a202639e77e26e903b76
deepanshu garg <dgarg6333@gmail.com>
2023-11-01 23:07:00 +0530

    responsive

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div className='flex p-6'>
+    <div className='md:flex p-6'>
         <div className='p-2 max-w-xl'>
             <button className='text-sky-700 text-lg font-bold py-1 px-4 border-solid border-2 border-blue-700 rounded-3xl mb-4'>FEATURED</button>
             <div className='flex bg-slate-100 p-4 border-solid border-2 border-slate-200 rounded-3xl'>
@@ -13,7 +13,7 @@ const Features = () => {
         <div className='md:m-32'></div>
         <div>
             <div className='p-2 max-w-xl'>
-                <button className='text-sky-700 text-lg font-bold py-1 px-4 border-solid border-2 border-blue-700 rounded-3xl mb-4'>VOLUNTEER WITH US</button>
+                <button className='text-sky-700 text-lg font-bold py-1 px-4 border-solid border-2 border-blue-700 rounded-3xl md:mb-4 sm:mb-2'>VOLUNTEER WITH US</button>
                 <div className='font-semibold text-justify p-2'>Just shoot an email to <a href='mailto:ieee.nitjsr@gmail.com' className='text-sky-800'>ieee.nitjsr@gmail.com</a> or message us in <a href='https://linkedin.com/' className='text-sky-800'>Linkedin</a> and we will get back to you ASAP </div>
             </div>
         </div>

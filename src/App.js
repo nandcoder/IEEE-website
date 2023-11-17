11eafd059e604ec9d5d0d2dbb5cad6ea6b51a150
sulochan <sulochankhadka25@gmail.com>
2023-11-17 22:44:42 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
-import HeroCarousel from './components/Carousel';
-import About from './components/About';
-import Navbar from './components/Navbar';
-import { Container } from 'react-bootstrap';
-import Footer from './components/Footer';
-import Aboutieee from './components/Aboutieee';
-import Event from './components/Event';
-import Features from './components/Features';
-
-=======
-import "./App.css";
-import Routes from "./routes";
-import { useRoutes } from "react-router-dom";
-import React from "react";
->>>>>>> main
+import Routes from './routes';
+import { useRoutes } from 'react-router-dom';
 function App() {
   let element = useRoutes(Routes);
   return (
     <React.Fragment>
-      <div className="App">{element} </div>
+      <div className='App'>{element} </div>
     </React.Fragment>
   );
 }

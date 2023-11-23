67ce5c3110228c32a87d199cb19784589e9dfb47
akashdnb <akashranjandnb@gmail.com>
2023-11-23 14:01:55 +0530

    integrated events api

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,7 +1,36 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import EventCard from "../components/EventCard";
 // import arrow from "../assets/icons8-arrow-24.png";
+
 function Events() {
+
+  const [event, setEvent] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://ieee-1vzv.onrender.com/api/events"
+        );
+        setEvent(response.data.events);
+        console.log("Successfully fetched ", response.data.events);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+
+  const handleSeeMore = () => {
+    alert('Button see more clicked with data:');
+  };
+
   return (
     <>
       <div
@@ -32,22 +61,14 @@ function Events() {
         >
           EVENTS
         </div>
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
+        {event.map((event, index) => (
+          <EventCard key={index} event={event} />
+        ))}
         <div>
           <button
             class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 my-4 flex items-center"
             style={{ borderRadius: "50px" }}
+            onClick={handleSeeMore}
           >
             See more
             <i class="fa fa-arrow-right ml-2"></i>

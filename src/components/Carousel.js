1760c34ad0062040d44965faa453dd2643010f11
Nand Raj <nandraj2001@gmail.com>
2023-11-24 11:08:17 +0530

    update carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import ExampleCarouselImage from './CarouselImage';
+import { Container } from 'react-bootstrap';
+import img1 from "../assets/ieee1.jpg"
+// import img2 from "../assets/ieee2.jpg"
+import img3 from "../assets/ieee3.jpg"
+import img4 from "../assets/ieee4.jpg"
 // import img from "../resources/bg.jpg"
 
 function ControlledCarousel() {
@@ -12,37 +17,38 @@ function ControlledCarousel() {
     const banners = [
         {
             id: 1,
-            title: "Advancing Technology for Humanity",
+            title: "Prof. Sivaji Chakravorti addresses NIT Jamshedpur students",
             description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
-            image: "https://wallpapers.com/images/hd/tree-on-green-lake-1519g7y2yexhnyvl.jpg",
+            image: img1,
         },
         {
             id: 2,
-            title: "Second slide label",
+            title: "IEEE Student Branch NIT Jamshedpur inauguration",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            image: "https://wallpapers.com/images/hd/tree-on-green-lake-1519g7y2yexhnyvl.jpg",
+            image: img3,
         },
         {
             id: 3,
-            title: "Third slide label",
+            title: "Advancing Technology for Humanity",
             description: "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
-            image: "https://wallpapers.com/images/hd/tree-on-green-lake-1519g7y2yexhnyvl.jpg",
+            image: img4,
         },
     ]
 
 
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-            {banners.map((banner) => (
-                <Carousel.Item key={banner.id}>
-                    <ExampleCarouselImage text={banner.title} image={banner.image} />
-                    <Carousel.Caption>
-                        <h3>{banner.title}</h3>
-                        <p>{banner.description}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            ))}
-        </Carousel>
+        <Container style={{ maxWidth: '80%' }}>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                {banners.map((banner) => (
+                    <Carousel.Item key={banner.id}>
+                        <ExampleCarouselImage text={banner.title} image={banner.image} />
+                        <Carousel.Caption style={{ fontSize: '2.25rem', background: 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))', width: 'inherit', left: 0, bottom: 0 }}>
+                            <p>{banner.title}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+        </Container>
     );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,10 @@ import Event from "../components/Event";
 import Features from "../components/Features";
 import HeroCarousel from "../components/Carousel";
 import About from "../components/About";
-import { Container } from "react-bootstrap";
 function Home() {
   return (
     <>
-      <Container>
-        <HeroCarousel />
-      </Container>
+      <HeroCarousel />
       <About />
       <Aboutieee />
       <Event />

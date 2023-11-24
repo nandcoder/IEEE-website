2d878a817060c3920fd9c017715bbbf35e4a6e05
Aditya Agrawal <2021ugee043@nitjsr.ac.in>
2023-11-24 16:59:26 +0530

    new modified navbar

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,49 +1,50 @@
-import { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from './CarouselImage';
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import ExampleCarouselImage from "./CarouselImage";
 // import img from "../resources/bg.jpg"
 
 function ControlledCarousel() {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex) => {
-        setIndex(selectedIndex);
-    };
-    const banners = [
-        {
-            id: 1,
-            title: "Advancing Technology for Humanity",
-            description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
-            image: "https://wallpapers.com/images/hd/tree-on-green-lake-1519g7y2yexhnyvl.jpg",
-        },
-        {
-            id: 2,
-            title: "Second slide label",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            image: "https://wallpapers.com/images/hd/tree-on-green-lake-1519g7y2yexhnyvl.jpg",
-        },
-        {
-            id: 3,
-            title: "Third slide label",
-            description: "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
-            image: "https://wallpapers.com/images/hd/tree-on-green-lake-1519g7y2yexhnyvl.jpg",
-        },
-    ]
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+  const banners = [
+    {
+      id: 1,
+      title:
+        "Prof Sivaji Chakravorty addresses NIT Jamshedpur students about IEEE membership",
+      description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      image: "1.webp",
+    },
+    {
+      id: 2,
+      title: " IEEE student branch NIT Jamshedpur core team",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      image: "2.webp",
+    },
+    {
+      id: 3,
+      title: "Third slide label",
+      description:
+        "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+      image: "3.webp",
+    },
+  ];
 
-
-    return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-            {banners.map((banner) => (
-                <Carousel.Item key={banner.id}>
-                    <ExampleCarouselImage text={banner.title} image={banner.image} />
-                    <Carousel.Caption>
-                        <h3>{banner.title}</h3>
-                        <p>{banner.description}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            ))}
-        </Carousel>
-    );
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      {banners.map((banner) => (
+        <Carousel.Item key={banner.id}>
+          <ExampleCarouselImage text={banner.title} image={banner.image} />
+          <Carousel.Caption>
+            <h3>{banner.title}</h3>
+            <p>{banner.description}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
 }
 
 export default ControlledCarousel;
